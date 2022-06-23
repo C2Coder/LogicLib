73445108fea3a220a35f187e17dc639ef5c122a4
TechnoCoder08 <102322220+TechnoCoder08@users.noreply.github.com>
2022-06-23 19:22:12 +0200

    update

--- logic.cpp
+++ logic.cpp
@@ -1,5 +1,7 @@
 #include "logic.h"
-struct {             // Structure declaration
-  int myNum;         // Member (int variable)
-  string myString;   // Member (string variable)
-} myStructure;       // Structure variable
+struct Person  
+{  
+    char name[30];  
+     int citizenship;  
+     int age;  
+}  
